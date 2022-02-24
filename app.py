25d65e61d985ca688a98c55e63cb247e40f0bcd5
Charles Samuel R <rcharles.samuel99@gmail.com>
2022-02-24 19:07:32 +0530

    Setting up function

--- app.py
+++ app.py
@@ -8,3 +8,21 @@ df = pd.read_csv("adult_trimmmed.csv")
 
 model = load_model('Income Prediction Model')
 
+# List of unique features in workclass column in df
+workclass_list = df['workclass'].unique()
+
+# List of unique features in education column in df
+education_list = df['education'].unique()
+
+marital_list = {"Single": 0, "Married": 1}
+
+# List of unique features in occupation column in df
+occupation_list = df['occupation'].unique()
+
+# List of unique features in relationship column in df
+relationship_list = df['relationship'].unique()
+
+sex_list = ["Male", "Female"]
+
+def income_predict(age, working_class, final_weight, education, marital_status, occupation, relationship, sex, capital_gain, capital_loss, hours_per_week):
+    pass
