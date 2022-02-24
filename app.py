2c3909ffaf105e63a5d4d6a614b347d34f809baf
Charles Samuel R <rcharles.samuel99@gmail.com>
2022-02-24 19:31:16 +0530

    Unecessary import and spelling

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from imp import load_module
 import gradio as gr
 import pandas as pd
 
 from pycaret.classification import *
 
-df = pd.read_csv("adult_trimmmed.csv")
+df = pd.read_csv("adult_trimmed.csv")
 
 model = load_model('Income Prediction Model')
 
