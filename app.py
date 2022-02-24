e696b7680652c41eb2eab652cd491dca2e66197b
Charles Samuel R <rcharles.samuel99@gmail.com>
2022-02-24 19:30:20 +0530

    Testing output

--- app.py
+++ app.py
@@ -15,7 +15,6 @@ workclass_list = df['workclass'].unique().tolist()
 education_list = df['education'].unique().tolist()
 
 marital_list = ["Single", "Married"]
-marital_dict = {0: "Single", 1: "Married"}
 
 # List of unique features in occupation column in df
 occupation_list = df['occupation'].unique().tolist()
@@ -32,12 +31,11 @@ def income_predict(age, working_class, final_weight, education, marital_status, 
 
     print(feats_s)
 
-    return feats_s
+    return "foo"
 
 
 gr.Interface(income_predict, inputs=[
     "number", gr.inputs.Dropdown(workclass_list), "number", gr.inputs.Dropdown(education_list), 
-    gr.inputs.Dropdown(marital_list), gr.inputs.Dropdown(occupation_list), gr.inputs.Dropdown(relationship_list), 
-    gr.inputs.Dropdown(sex_list), 
-
-])
+    gr.inputs.Dropdown(marital_list, type="index"), gr.inputs.Dropdown(occupation_list), gr.inputs.Dropdown(relationship_list), 
+    gr.inputs.Dropdown(sex_list), "number", "number", "number"], 
+    outputs="text").launch()
